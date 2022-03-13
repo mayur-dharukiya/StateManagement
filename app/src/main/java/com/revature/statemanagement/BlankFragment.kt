3f743e21c25512327a61ad8a5b4885f76216be2a
mayur-dharukiya <mayur@revature.com>
2022-03-13 16:50:45 -0400

    Statement Management Completed

---
+++ app/src/main/java/com/revature/statemanagement/BlankFragment.kt
@@ -0,0 +1,37 @@
+package com.revature.statemanagement
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Text(text = "Hello world.")
+            }
+    }
+
+    }
+}
--- app/src/main/java/com/revature/statemanagement/MainActivity.kt
+++ app/src/main/java/com/revature/statemanagement/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.revature.statemanagement
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.revature.statemanagement.ui.theme.StateManagementTheme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             StateManagementTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,10 +30,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
-                    Greeting("Android")
-                     Greeting("Android")
-                    Greeting("Android")
+                  //  Greeting("Android")
+
+                   RememberSample()
+                   // RememberFragment()
+
 
                 }
             }
@@ -33,6 +42,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Composable
+fun RememberSample() {
+    var   clickCount by rememberSaveable { mutableStateOf(0) }
+    Column {
+        Button(onClick = { clickCount++ }) {
+            Text(text = "" + clickCount + " times clicked")
+        }
+    }
+}
+
+@Composable
+fun RememberFragment() {
+    var   clickCount by rememberSaveable { mutableStateOf(0) }
+
+    var context= LocalContext.current
+
+    Column {
+        Button(onClick = { context.startActivity(Intent(context,BlankFragment::class.java))}) {
+            Text(text =  " Go to Fragment")
+        }
+    }
+}
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
